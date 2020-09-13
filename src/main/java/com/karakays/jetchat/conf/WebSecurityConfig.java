f6d580c0db5c46799b8bd0ac2283ae7c9085e705
Selçuk Karakayalı <skarakayali@gmail.com>
2020-09-13 14:11:37 +0300

    - polish

--- src/main/java/com/karakays/jetchat/conf/WebSecurityConfig.java
+++ src/main/java/com/karakays/jetchat/conf/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.karakays.jetchat.conf;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,4 +45,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
